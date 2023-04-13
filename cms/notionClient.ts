a1b1fc56d79f20528d3df99cec697bb235b9f7de
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-04-13 22:52:38 +0900

    Feat: Init unofficial notion client sdk to render notion page on client w/ react-notion-x

--- cms/notionClient.ts
+++ cms/notionClient.ts
@@ -1,6 +1,8 @@
 import { Client } from '@notionhq/client';
+import { NotionAPI } from 'notion-client';
 
 // 무조건 서버사이드에서만 실행된다
+/** 공식 노션 Sdk 인스턴스 */
 export const notionClient = new Client({
   auth: process.env.NOTION_TOKEN,
 });
@@ -23,4 +25,12 @@ export const getDatabaseItems = async (databaseId: string) => {
   });
 
   return response.results;
+};
+
+/** 비공식 노션 Sdk 인스턴스 - react-notion-x로 노션 페이지 렌더링을 위해 사용 */
+export const notion = new NotionAPI();
+
+export const getPageContent = async (pageId: string) => {
+  const recordMap = await notion.getPage(pageId);
+  return recordMap;
 };
