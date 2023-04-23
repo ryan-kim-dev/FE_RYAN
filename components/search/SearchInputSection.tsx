5d75dc6c50eb6e1461d4c6053a1b4d1765869d32
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-04-23 23:38:07 +0900

    Feat: Implement function handles user input to query notion db

--- components/search/SearchInputSection.tsx
+++ components/search/SearchInputSection.tsx
@@ -1,11 +1,30 @@
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 
 const SearchInputSection = () => {
+  const { push } = useRouter();
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const trimmedValue = searchValue.trim(); // 검색어 쿼리의 공백 제거
+    if (!trimmedValue) return alert('검색어를 입력해주세요.');
+    // https://frontdev.tistory.com/entry/BASE-20%EC%9D%B4%EB%9E%80
+
+    push(`/search?query=${trimmedValue}`);
+  };
+
   return (
     <section className="bg-black">
       <div className="w-4/5 max-w-5xl mx-auto py-16">
-        <form className="relative">
-          <input type="text" className="w-full outline-none p-2 text-xl" />
+        <form onSubmit={handleSubmit} className="relative">
+          <input
+            type="text"
+            onChange={(e) => setSearchValue(e.target.value)}
+            className="w-full outline-none p-2 text-xl"
+          />
           <button
             type="submit"
             className="absolute right-2 top-1/2 -translate-y-1/2 hover:bg-gray-200 hover:rounded-full hover:p-1 transition duration-2"
