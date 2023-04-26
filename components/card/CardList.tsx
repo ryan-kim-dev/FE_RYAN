cd380f6a99da7e968745a72557caee1e1e652058
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-04-26 21:00:42 +0900

    Feat: Add conditions relate to no search results & no items for card list component

--- components/card/CardList.tsx
+++ components/card/CardList.tsx
@@ -6,6 +6,12 @@ interface CardListProps {
 }
 
 const CardList = ({ cardItems }: CardListProps) => {
+  if (cardItems.length === 0)
+    return (
+      <div className="w-4/5 max-w-5xl mx-auto my-16">
+        <p className="text-center text-2xl">검색 결과가 없습니다.</p>
+      </div>
+    );
   return (
     <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       {cardItems.map((cardItem) => (
