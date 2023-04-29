acaeae3255d7570cef0ff46a75d5ebd3158151f8
ryan-kim-dev <ryankim.h.dev@gmail.com>
2023-04-29 23:46:37 +0900

    Feat: Add contact links of other social network services

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,5 +1,30 @@
+import Link from 'next/link';
+import { AiFillLinkedin, AiFillGithub, AiFillYoutube } from 'react-icons/ai';
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div>
+      <h1>About Me</h1>
+      <div>작업중...</div>
+      <ul>
+        <li>
+          <Link href="https://www.linkedin.com/in/ryankim-fe">
+            <AiFillLinkedin />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://github.com/ryan-kim-dev">
+            <AiFillGithub />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.youtube.com/@feryan8247">
+            <AiFillYoutube />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default AboutPage;
